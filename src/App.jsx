0759747ef44067cd4658b619b52eb0fc452075ea
Muhammad Andre Widianto <andrewidianto12@gmail.com>
2024-03-23 11:50:52 +0700

    nambahin replay error

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ import { BrowserTracing } from "@sentry/tracing";
 import viteLogo from "/vite.svg";
 
 let release = "react-sentry-github@1.0.0";
+
 Sentry.init({
   dsn: "https://cbaec468c541fb9b8465856260838b52@o4506948520837120.ingest.us.sentry.io/4506948853104645",
-  integrations: [new BrowserTracing()],
+  integrations: [new BrowserTracing(), Sentry.replayIntegration()],
   release: release,
   //
   //
   //
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 });
 
@@ -45,7 +49,7 @@ function App() {
         <button
           onClick={() => {
             setCount((count) => count + 1);
-            handleclick("Error pantekkk");
+            handleclick("Error brooo");
           }}
         >
           count is {count}
